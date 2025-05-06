177fcc9ebbdb4dbba3d012c31f5d10cfc100c884
Zain <zain@Zains-MacBook-Air.local>
2025-05-07 00:25:18 +0100

    gone real bad

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css';
+import WhiteLogo from './assets/Z - White text.png';
+import WhiteURL from './assets/z website name white.png';
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <h1></h1>
+      </header>
+      <main className="main-content">
+        <img
+          src={WhiteLogo}
+          alt="Zain Logo"
+          className="zain"
+        />
+        <div className="matrix-text">
+          {Array.from({ length: 1000 }).map((_, i) => (
+            <span
+              key={i}
+              className="woo"
+              style={{ animationDelay: `${Math.random() * 5}s` }} // Random delay
+            >
+              woo
+            </span>
+          ))}
+        </div>
+      </main>
+      <footer className="footer">
+        <img src={WhiteURL} alt="Zain URL" className="website" />
+      </footer>
+    </>
+  );
+}
+
+export default App;
