223ffa0c02e27cdb0c934eb35c0713126f5e51bd
Zain <zain@Zains-MacBook-Air.local>
2025-05-13 21:51:56 +0100

    updating sub module

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import ghPages from 'vite-plugin-gh-pages';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/thisiszain/' : '/',
+  plugins: [react(), ghPages()],
+}));
