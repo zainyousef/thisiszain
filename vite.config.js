2a312708d0bcfdd42623727851a9f3706c55ceed
zainyousef <zainyousef@me.com>
2025-05-13 23:14:27 +0100

    Remove submodule and move files to root

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { ghPages } from 'vite-plugin-gh-pages';
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/thisiszain/' : '/', // Replace 'thisiszain' with your repo name
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/thisiszain/', // Replace 'thisiszain' with your repository name
   plugins: [react(), ghPages()],
-}));
+});
