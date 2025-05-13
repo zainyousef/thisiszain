b9a809aa7399b5fad2d5fe5d570e2941c3187457
zainyousef <zainyousef@me.com>
2025-05-14 00:14:12 +0100

    alalala

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/thisiszain/', // Replace 'thisiszain' with your repository name
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   build: {
     outDir: 'dist', // Specify the output directory
   },
