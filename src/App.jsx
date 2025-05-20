e03abfbddb8c8b85b51d606bff518fdb3ce9b106
zainyousef <zainyousef@me.com>
2025-05-20 20:56:42 +0100

    making this shit great

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import WhiteLogo from './assets/Z - White text.png';
 import WhiteURL from './assets/z website name white.png';
+import React from 'react';
 
 function App() {
   return (
@@ -9,17 +10,16 @@ function App() {
         <h1></h1>
       </header>
       <main className="main-content">
-        <img
-          src={WhiteLogo}
-          alt="Zain Logo"
-          className="zain"
-        />
+        <img src={WhiteLogo} alt="Zain Logo" className="zain" />
         <div className="matrix-text">
           {Array.from({ length: 1000 }).map((_, i) => (
             <span
               key={i}
               className="woo"
-              style={{ animationDelay: `${Math.random() * 5}s` }} // Random delay
+              style={{
+                animationDelay: `${Math.random() * 5}s`, // Shorter delay
+                animationDuration: `${Math.random() * 5 + 5}s`, // Varying duration
+              }}
             >
               woo
             </span>
