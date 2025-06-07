ac2fd61f2ea6663a71b5723fcf8f9d11e1d3b699
zainyousef <zainyousef@me.com>
2025-06-07 11:48:30 +0100

    removing file path

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/thisiszain/', // Replace 'thisiszain' with your repository name
+  base: '/', // Replace 'thisiszain' with your repository name
   plugins: [react()],
   build: {
-    outDir: 'dist', // Specify the output directory
+    outDir: 'dist', // Specify the output diectory dd
   },
 });
